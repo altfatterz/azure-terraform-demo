e0f7599da226d6d2e407e387c7a6c0c4242c172d
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2023-04-01 22:06:16 +0200

    create vm

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "resource_group_location" {
   default     = "westeurope"
   description = "Location of the resource group."
 }
+
+variable "hostname" {
+  default     = "myVM"
+  description = "Specifies the Hostname which should be used for this Virtual Machine."
+}
+
+variable "admin_username" {
+  default     = "ubuntu"
+  description = "The username of the local administrator used for the Virtual Machine."
+}
